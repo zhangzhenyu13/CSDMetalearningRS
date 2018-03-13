74b00ad1f864b80cc409f80ec1a708e0bbf10fb8
bird <zhangzhenyu13@outlook.com>
2018-03-13 14:58:27 +0800

    model test

--- ML_Models/Model_def.py
+++ ML_Models/Model_def.py
@@ -1,5 +1,5 @@
 import pickle
-import json
+import numpy as np
 class ML_model:
     def __init__(self):
         self.model=None
@@ -17,17 +17,46 @@ class ML_model:
         with open("../data/saved_ML_models/"+self.name+".pkl","rb") as f:
             data=pickle.load(f)
             self.model=data["model"]
+            self.name=data["name"]
     def saveModel(self):
         with open("../data/saved_ML_models/"+self.name+".pkl","wb") as f:
             data={}
             data["model"]=self.model
+            data["name"]=self.name
             pickle.dump(data,f)
 
 class DataSetTopcoder:
     def __init__(self):
+        self.dataSet=None
         self.trainX=None
         self.trainLabel=None
         self.testX=None
         self.testLabel=None
-    def loadData(self,cluster_no):
-        pass
+        self.splitRatio=0.8
+        self.loadData()
+
+    def loadData(self,choice=1):
+        with open("../data/Instances/task_user"+str(choice)+".data","rb") as f:
+            self.dataSet=pickle.load(f)
+        users=self.dataSet["users"]
+        tasks=self.dataSet["tasks"]
+        X=np.concatenate((tasks,users),axis=1)
+        self.trainSize=int(self.splitRatio*len(X))
+        self.trainX=X[:self.trainSize]
+        self.testX=X[self.trainSize:]
+        print("feature length for user(%d) and task(%d) is %d"%(len(users[0]),len(tasks[0]),len(X[0])))
+        print("loaded all the instances, size=%d"%len(X),"trainSize=%d"%self.trainSize)
+
+    def CommitRegressionData(self):
+        Y=np.array(self.dataSet["submits"])
+        self.trainLabel=Y[:self.trainSize]
+        self.testLabel=Y[self.trainSize:]
+    def CommitClassificationData(self):
+        Y=np.array(self.dataSet["submits"])
+        Y=np.array(Y,dtype=np.int)
+        self.trainLabel=Y[:self.trainSize]
+        self.testLabel=Y[self.trainSize:]
+    def WinRankData(self):
+        Y=np.array(self.dataSet["ranks"])
+        self.trainLabel=Y[:self.trainSize]
+        self.testLabel=Y[self.trainSize:]
---
+++ ML_Models/TraditionalModel.py
@@ -0,0 +1,71 @@
+from ML_Models.Model_def import *
+from sklearn import svm,linear_model,tree
+from sklearn import metrics
+import time
+
+#define set of regressor
+regressor={
+    "svr":svm.SVR()
+}
+#define set of classifier
+classifier={
+    "svc":svm.SVC()
+}
+#model container
+class TraditionalRegressor(ML_model):
+    def __init__(self,regressor):
+        ML_model.__init__(self)
+        self.model=regressor
+    def predict(self,X):
+        print(self.name,"is predicting")
+        Y=self.model.predict(X)
+        return Y
+    def trainModel(self):
+        print("training")
+        t0=time.time()
+        self.model.fit(self.dataSet.trainX,self.dataSet.trainLabel)
+        t1=time.time()
+        mse=metrics.mean_squared_error(self.dataSet.trainLabel,self.model.predict(self.dataSet.trainX))
+        print("model",self.name,"training finished in %ds"%(t1-t0),"train mse=%f"%mse)
+
+class TraditionalClassifier(ML_model):
+    def __init__(self,classifier):
+        ML_model.__init__(self)
+        self.model=classifier
+    def predict(self,X):
+        print(self.name,"is predicting")
+        Y=self.model.predict(X)
+        return Y
+    def trainModel(self):
+        print("training")
+        t0=time.time()
+        self.model.fit(self.dataSet.trainX,self.dataSet.trainLabel)
+        t1=time.time()
+        score=metrics.accuracy_score(self.dataSet.trainLabel,self.model.predict(self.dataSet.trainX))
+        print("model",self.name,"trainning finished in %ds"%(t1-t0),"train score=%f"%score)
+
+#test the performance
+if __name__ == '__main__':
+
+    data=DataSetTopcoder()
+    #regression
+    data.CommitRegressionData()
+    model=TraditionalRegressor(linear_model.LinearRegression())
+    model.name="linearRegressorCommit"
+    model.dataSet=data
+    model.trainModel()
+    model.saveModel()
+    model.loadModel()
+    Y_predict=model.predict(data.testX)
+    print("test mse=%f"%(metrics.mean_squared_error(data.testLabel,Y_predict)))
+
+    #classification
+    data.CommitClassificationData()
+    model=TraditionalClassifier(linear_model.SGDClassifier())
+    model.dataSet=data
+    model.name="linearClassifierCommit"
+    model.trainModel()
+    model.saveModel()
+    model.loadModel()
+    Y_predict=model.predict(data.testX)
+    print("test score=%f"%(metrics.accuracy_score(data.testLabel,Y_predict)))
